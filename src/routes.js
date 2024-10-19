d7e8e54a72c0bd2d3430cc5ef5f3965ebeaca09b
Sindhu P R <sindhupr2003@gmail.com>
2024-10-20 01:20:03 +0530

    bot backend

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const chatController = require('./controllers/chatController');
+const router = express.Router();
+
+router.post('/chat', chatController.handleChat);
+
+router.post('/translate', chatController.handleTranslate);
+
+module.exports = router;
